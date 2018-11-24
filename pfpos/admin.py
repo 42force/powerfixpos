0d01ad15f2095a449e7d7d990ef4461c2568cb49
Chobe <42force@gmail.com>
2018-11-24 17:51:24 +0800

    check db if this will connect

--- pfpos/admin.py
+++ pfpos/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Product, Cash, Order, Setting
+
+from django.contrib.admin import SimpleListFilter
+
+admin.site.site_header = "Powerfix Administration"
+admin.site.site_title = "Powerfix Administration"
+
+
+class SettingAdmin(admin.ModelAdmin):
+
+    list_display=(
+        'key',
+        'value'
+    )
+
+
+#custom Filter for ProductAdmin
+class LowStockFilter(SimpleListFilter):
+
+    
+
+
+admin.site.register(Cash)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Setting)
