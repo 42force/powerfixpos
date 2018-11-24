55678405e1b129503d3a70b0ac51b3346d89f137
Chobe <42force@gmail.com>
2018-11-24 18:05:10 +0800

    changed in Admin info wth fields.

--- pfpos/admin.py
+++ pfpos/admin.py
@@ -19,8 +19,76 @@ class SettingAdmin(admin.ModelAdmin):
 #custom Filter for ProductAdmin
 class LowStockFilter(SimpleListFilter):
 
-    
+    title = 'Available Stocks'
+    parameter_name = 'stock_available'
 
+    def lookups(self, request, model_admin):
+
+        return(
+            ('low', 'Low Stock'),
+            ('high', 'High Stock' )
+        )
+
+    def queryset(self, request, queryset):
+
+
+        if self.value() == 'low':
+            return queryset.filter(stock__lte=F('minimum_stock'))
+        if self.value() == 'high':
+            return queryset.filter(stock__gte=F('minimum_stock'))
+
+
+class ProductAdmin(admin.ModelAdmin):
+    actions = ['delete_selected']
+
+
+    list_display = (
+        'name',
+        'description',
+        'code',
+        'price',
+        'stock',
+        'minimum_stock',
+        'stock_applies'
+    )
+
+    list_display_links = (
+        'code',
+        'name',
+    )
+
+    search_fields = (
+        'code',
+        'name'
+
+    )
+
+    list_filter = (
+        LowStockFilter,
+    )
+
+def safe_delete_order(modeladmin, request, queryset):
+        queryset.filter(done=True).delete()
+        safe_delete_order.short_description = "Safely Delete Completed Orders"
+
+
+class OrderAdmin(admin.ModelAdmin):
+
+    actions = [safe_delete_order]
+
+    list_display = (
+        'user',
+        'total_price',
+        'done',
+        'last_change'
+    )
+
+    #sidebar Filter
+    list_filter = (
+        'done'
+    )
+
+    ordering = ('done',)
 
 admin.site.register(Cash)
 admin.site.register(Product)
